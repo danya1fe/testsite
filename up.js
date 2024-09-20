9dd77dcc104a77b2bcfd6247403be1edeb7d95d3
danya1fe <danyafe@icloud.com>
2024-09-20 15:58:40 +0200

    first commit

---
+++ up.js
@@ -0,0 +1,64 @@
+// console.log("Wsp")
+
+// const b=5
+// let c=10
+// c=5
+// console.log(c)
+// const COLOR_RED="#FFFFFF"
+// const myName="sdsd"
+// let isChecked=true
+// let biggest=4>5
+// console.log(biggest)
+// const age=null
+// let name
+// console.log(name)
+// console.log(typeof 5)
+// console.log(typeof(50+"off"))
+// console.log(Math.random(4,5))
+// console.log(`hello ${myName}`)
+// c++
+// c--
+// ++c
+// console.log(c)
+// let counter=1
+// let a=++counter
+// console.log(a)
+// let counter2=1
+// let f=counter2++
+// console.log(2!=1)
+// console.log(`hello ${myName}`)
+// alert(2>2)
+// alert ("Hello")
+// let result= prompt("Your name")
+// console.log(result)
+// let human = confirm("Are you human?")
+// alert(human)
+// let a = 5
+// if (a==4){
+// alert("corectly")
+// }
+// if(a==5) alert("abc")
+//     const year=prompt("Which year is now?")
+// if (year==2024){
+// alert("corectly")
+// }else if(year==0){
+// alert("you guessed the secret year") 
+// }
+//  else{
+//     alert("not corectly")
+// }
+// let success=(a>2)? true : false
+
+// if (a>2){
+
+// }
+// || или |or возврощает первое истиное значения
+// && и |and возврощает первое ложное значения
+// приоретет оператора И выше чем у или A && B || C && D
+// ! NOT|НЕ 
+
+let result = 1 && 0 && 7
+console.log (result)
+
+
+
